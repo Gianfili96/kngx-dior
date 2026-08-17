4c87f4fcb53e5720294b4153969582e2ea304644
Gianpiero <mitico0096@gmail.com>
2026-08-17 12:59:53 +0000

    Fix type=module negli script e aggiunta vite.config.js per multi-page build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        licensing: resolve(__dirname, 'licensing.html'),
+      },
+    },
+  },
+})
